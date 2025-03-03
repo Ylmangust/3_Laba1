8d89a556ae172cadc9b0659c95d81e96eea3765a
Ylmangust <adeli-2019@mail.ru>
2025-03-03 14:11:02 +0300

    1st_commit

---
+++ Lab1/src/main/java/mephi/b22901/lab1/Lab1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b22901.lab1;
+
+/**
+ *
+ * @author Регина
+ */
+public class Lab1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
